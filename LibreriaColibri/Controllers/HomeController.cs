f812f26c35c66c7a9a48ffd094474b4720934aaf
Alexis <vegacastro97@gmail.com>
2022-04-26 10:54:41 -0500

    vista home agregada

--- LibreriaColibri/Controllers/HomeController.cs
+++ LibreriaColibri/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace LibreriaColibri.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Home");
         }
 
         public IActionResult Privacy()
