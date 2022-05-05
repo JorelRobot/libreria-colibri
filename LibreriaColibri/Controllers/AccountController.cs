e76a21b9ec0de782b0c477c81f87db3faae8eed6
Fernando Sustaita <fernandojsustaita@gmail.com>
2022-05-05 12:16:13 -0500

    Modificacion de vista de perfil para que aparescan los datos, ya funcional

--- LibreriaColibri/Controllers/AccountController.cs
+++ LibreriaColibri/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using LibreriaColibri.Data;
+using LibreriaColibri.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibreriaColibri.Controllers
@@ -6,10 +8,12 @@ namespace LibreriaColibri.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly LibraryContext _context;
         
-        public AccountController(UserManager<IdentityUser> userManager)
+        public AccountController(UserManager<IdentityUser> userManager, LibraryContext context)
         {
             this._userManager = userManager;
+            this._context = context;
         }
         public IActionResult Index()
         {
@@ -17,9 +21,51 @@ namespace LibreriaColibri.Controllers
         }
 
         [HttpGet]
-        public IActionResult MyProfile()
+        public async Task<IActionResult> MyProfile()
         {
-            return View();
+            ProfileViewModel model = new ProfileViewModel();
+            var id =  _userManager.GetUserId(User);
+            if (id == null)
+            {
+                NotFound();
+            }
+            var currentUser = await _context.Usuario.FindAsync(id);
+            if(currentUser == null)
+            {
+                NotFound();
+            }
+            model.Id = currentUser.Id;
+            model.City = currentUser.City;
+            model.Address = currentUser.Address;
+            model.Phone = currentUser.PhoneNumber;
+            model.ZipCode = currentUser.ZipCode;
+            model.Country = currentUser.Country;
+            model.Name = currentUser.Name;
+            model.Email = currentUser.Email;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MyProfile(ProfileViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var currentUser = await _context.Usuario.FindAsync(model.Id);
+
+                currentUser.Id = model.Id;
+                currentUser.City = model.City;
+                currentUser.Address = model.Address;
+                currentUser.PhoneNumber = model.Phone;
+                currentUser.ZipCode = model.ZipCode;
+                currentUser.Country = model.Country;
+                currentUser.Name = model.Name;
+                currentUser.Email = model.Email;
+
+                await _userManager.UpdateAsync(currentUser);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
         }
     }
 }
